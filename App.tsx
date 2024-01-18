b024d14903fe3d63e5b3b261be317262ffe8bb60
Muzammil Hussain Shah <mynameismuzammilhussainshah@gmail.com>
2024-01-19 03:01:23 +0500

    quick payment method [ADD]

--- App.tsx
+++ App.tsx
@@ -1,6 +1,44 @@
 import React, { useEffect } from 'react';
 import { View, Button } from 'react-native';
 import { SQIPCore, SQIPCardEntry } from 'react-native-square-in-app-payments';
+import axios from 'react-native-axios'
+
+
+export default async function chargeCardNonce(
+  nonce: string,
+  verificationToken = undefined,
+) {
+  let body;
+  if (verificationToken === undefined) {
+    body = JSON.stringify({
+      nonce,
+    });
+  } else {
+    body = JSON.stringify({
+      nonce,
+      verificationToken,
+    });
+  }
+  const response = await fetch(`http://192.168.0.33:3002/chargeForCookie`, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body,
+  });
+
+  try {
+    const responseJson = await response.json();
+    console.log(responseJson, 'responseJson')
+    if (responseJson.errorMessage != null) {
+      console.error(responseJson.errorMessage);
+    }
+  } catch (error: any) {
+    console.error(error.message);
+  }
+}
+
 
 const App = () => {
   useEffect(() => {
@@ -12,6 +50,58 @@ const App = () => {
     initializeSquare();
   }, []);
 
+
+  // const callApi = async () => {
+  //   var raw = "";
+
+  //   var requestOptions = {
+  //     method: 'POST',
+  //     body: raw,
+  //     redirect: 'follow'
+  //   };
+
+  //   fetch("http://localhost:3001/chargeForCookie", requestOptions)
+  //     .then(response => response.text())
+  //     .then(result => console.log(result))
+  //     .catch(error => console.log('error', error));
+  // };
+
+
+  const callApi = async () => {
+    try {
+      // console.log("client")
+      // // Replace 'localhost' with the actual IP address or hostname of your API server
+      // const apiUrl = 'http://192.168.0.33:3002/chargeForCookie';
+
+      // const response = await axios.post(apiUrl, {
+      //   // You can include request parameters here
+      //   // key: 'value',
+      // });
+      // console.log("response", response)
+
+      // 
+      // setResult(response.data);
+      // await chargeCardNonce(
+      //   buyerVerificationDetails.nonce,
+      //   buyerVerificationDetails.token,
+      // );
+    } catch (error) {
+      console.error('Error calling API:', error);
+    }
+  };
+
+
+
+
+
+
+
+
+
+
+
+
+
   const onCardEntryComplete = () => {
     // Update UI to notify user that the payment flow is completed
   };
@@ -19,12 +109,12 @@ const App = () => {
   const onCardNonceRequestSuccess = async (cardDetails) => {
     try {
 
-      console.log(cardDetails,'cardDetailscardDetails')
+      // console.log(cardDetails, 'cardDetailscardDetails')
 
 
+      await chargeCardNonce(cardDetails?.nonce);
 
 
-      
       // take payment with the card details
       // await chargeCard(cardDetails);
 
